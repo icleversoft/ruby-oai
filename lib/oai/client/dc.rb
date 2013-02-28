fbcd2d95b49a28a3fe55222aa02c6fff75f83890
George Stavrou <iphone@icleversoft.com>
2013-02-28 12:49:36 +0200

    arg to string

--- lib/oai/client/dc.rb
+++ lib/oai/client/dc.rb
@@ -2,7 +2,7 @@ module OAI
   require 'rexml/document'
   class Dc 
     def initialize( doc )
-      doc = REXML::Document.new(doc).root
+      doc = REXML::Document.new(doc.to_s).root
       #Bypass first elemet
       doc.elements.first.each do |elm|
         p elm.name

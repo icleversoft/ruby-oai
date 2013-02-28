3cea82096676529bba6c23ad90d2accc84feb989
George Stavrou <iphone@icleversoft.com>
2013-02-28 12:53:41 +0200

    attrs on dc

--- lib/oai/client/dc.rb
+++ lib/oai/client/dc.rb
@@ -1,11 +1,13 @@
 module OAI
   require 'rexml/document'
   class Dc 
+    attr_accessor :doc, :elms
     def initialize( doc )
-      doc = REXML::Document.new(doc.to_s).root
+      @doc = REXML::Document.new(doc.to_s).root
+      @elms = []
       #Bypass first elemet
-      doc.elements.first.each do |elm|
-        p elm.name
+      @doc.elements.first.each do |elm|
+        @elms << elm  
       end
     end
   end

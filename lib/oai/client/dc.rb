fbf257c0e17c9de72d86ba3bcbc5cf453c5c85a4
George Stavrou <iphone@icleversoft.com>
2013-02-28 14:30:24 +0200

    typo fixed

--- lib/oai/client/dc.rb
+++ lib/oai/client/dc.rb
@@ -39,14 +39,14 @@ module OAI
 
     def build_attributes
       #Define dynamic methods
-      create_method("#{name}=".to_sym) do |val|
-        instance_variable_set("@" + name, value)
-      end
-      create_method( name.to_sym ) do
-        instance_variable_get("@" + name)
-      end
       @elms.each do |name, value|
-        self.instance_variable_set("@#{name}", value)
+        create_method("#{name}=".to_sym) do |val|
+          instance_variable_set("@" + name, val)
+        end
+        create_method( name.to_sym ) do
+          instance_variable_get("@" + name)
+        end
+        self.instance_variable_set("@" + name, value)
       end
     end
   end

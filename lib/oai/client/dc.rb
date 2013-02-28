f3b38b718e7e8aefe90f2400248102ff88e71d05
George Stavrou <iphone@icleversoft.com>
2013-02-28 12:44:15 +0200

    typo fixed

--- lib/oai/client/dc.rb
+++ lib/oai/client/dc.rb
@@ -2,7 +2,7 @@ module OAI
   require 'rexml/document'
   class Dc 
     def initialize( doc )
-      doc = RECML::Document.new(doc).root
+      doc = REXML::Document.new(doc).root
       #Bypass first elemet
       doc.elements.first.each do |elm|
         p elm.name
